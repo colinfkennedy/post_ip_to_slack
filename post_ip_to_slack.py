9e0bfee1630634e5f2cee0c25819672e9f3d9d7d
Colin Kennedy <colin.kennedy@teamaol.com>
2016-10-21 11:45:14 +0100

    Removing sleep

--- post_ip_to_slack.py
+++ post_ip_to_slack.py
@@ -3,11 +3,7 @@ import slackweb
 slack = slackweb.Slack(url="<Your_Slack_Hook_URL>")
 message = "RPi has started up. Ip addresses:\n"
 
-time.sleep(5)
 print "Finding IP addresses"
-while ni.ifaddresses('wlan0').get(ni.AF_INET)[0].get('addr') is None:
-    print "Waiting for WLan to get an IP. Sleeping for 5 seconds."
-
 
 for interface in ni.interfaces():
     addresses = ni.ifaddresses(interface)
